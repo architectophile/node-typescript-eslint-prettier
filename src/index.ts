1041f08361261b10d0cff4ae4bf1cab8f55d6240
Byeongcheol Yoo <architectophile@gmail.com>
2020-08-27 14:04:24 +0900

    Debug mode added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { APP_NAME } from "./constants";
+
 console.log("Hello World! This is the first TypeScript project!");
+console.log("This is " + APP_NAME);
 
 for (let i = 0; i < 100; i++) {
   console.log(i);
